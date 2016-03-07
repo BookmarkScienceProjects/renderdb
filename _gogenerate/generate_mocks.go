f3e2b12f82d4b7f14bafb5dc8a0f2d7b59fa2f7e
Lars Moastuen <lars@rendra.io>
2016-03-08 00:06:55 +0100

    Auto-generate mocks for everything.

---
+++ _gogenerate/generate_mocks.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+)
+
+func main() {
+	directories, err := glideNoVendor()
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("Could not run 'glide nv': %s\n", err))
+		os.Exit(1)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(directories))
+	for _, d := range directories {
+		dir := d
+		go func() {
+			defer wg.Done()
+			fmt.Printf("Generating mocks for '%s'...\n", dir)
+			err = generateMocks(dir)
+			if err != nil {
+				os.Stderr.WriteString(fmt.Sprintf("Could not generate mocks for directory '%s': %s\n", dir, err))
+				os.Exit(2)
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func glideNoVendor() ([]string, error) {
+	cmd := exec.Command("glide", "nv")
+	bytes, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(bytes), "\n")
+	filtered := make([]string, 0, len(lines))
+	for _, l := range lines {
+		line := strings.TrimSuffix(l, "...")
+		if line != "" {
+			filtered = append(filtered, line)
+		}
+	}
+	return filtered, err
+}
+
+func generateMocks(dir string) error {
+	args := []string{"-dir", dir, "-inpkg", "-testonly", "-all"}
+	if dir != "." {
+		args = append(args, "-recursive")
+	}
+	cmd := exec.Command("mockery", args...)
+	return cmd.Run()
+}
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+//go:generate go run _gogenerate/generate_mocks.go
+
 import (
 	"errors"
 	"flag"
